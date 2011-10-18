1f068ec126ecc9cdfae4d78d57a29f312d3b33f5
Laurent Sansonetti <lrz@chopine.be>
2011-10-18 17:13:59 +0200

    add paint sample

---
+++ sample/paint/app/main.rb
@@ -0,0 +1,69 @@
+class PaintView < UIView
+  def initWithFrame(ect)
+    if super
+      @paths = []
+    end
+    self
+  end
+
+  def drawRect(rect)
+    UIColor.blackColor.set
+    UIBezierPath.bezierPathWithRect(rect).fill
+    @paths.each do |path, color|
+      color.set
+      path.stroke
+    end
+  end
+
+  def touchesBegan(touches, withEvent:event)
+    bp = UIBezierPath.alloc.init#bezierPath
+    bp.lineWidth = 3.0
+    random_color = begin
+      red, green, blue = rand(100), rand(100), rand(100)
+      UIColor.alloc.initWithRed(red/100.0, green:green/100.0, blue:blue/100.0, alpha:1.0)
+    end
+    @paths << [bp, random_color]
+  end
+
+  def touchesMoved(touches, withEvent:event)
+    touch = event.touchesForView(self).anyObject
+    point = touch.locationInView(self)
+    if @previousPoint and !@paths.empty?
+      bp = @paths.last.first
+      bp.moveToPoint(@previousPoint)
+      bp.addLineToPoint(point)
+    end
+    @previousPoint = point
+    setNeedsDisplay
+  end
+
+  def touchesEnded(touches, withEvent:event)
+    @previousPoint = nil
+  end
+
+  def canBecomeFirstResponder
+    true
+  end
+
+  def motionEnded(motion, withEvent:event)
+    if motion == UIEventSubtypeMotionShake
+      @paths.clear
+      setNeedsDisplay
+    end
+  end
+end
+
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+
+    pvrect = window.bounds
+    pv = PaintView.alloc.initWithFrame(pvrect)
+    pv.multipleTouchEnabled = true
+    pv.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth
+    window.addSubview(pv)
+    pv.becomeFirstResponder
+
+    window.makeKeyAndVisible
+  end
+end
