b937abce582865851f1c47823c1def1b816465b6
Laurent Sansonetti <lrz@chopine.be>
2011-12-16 13:01:33 +0100

    merge mapview and tableview samples

---
+++ sample/mapview/app/beer_list.rb
@@ -0,0 +1,40 @@
+class BeerList < UITableViewController
+  def init
+    if super
+      self.tabBarItem = UITabBarItem.alloc.initWithTitle('List', image:nil, tag:1)
+    end
+    self
+  end
+
+  def viewDidLoad
+    view.dataSource = view.delegate = self
+  end
+
+  def viewWillAppear(animated)
+    navigationController.setNavigationBarHidden(true, animated:false)
+  end    
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    Beer::All.size
+  end
+
+  CELLID = 'CellIdentifier'
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier(CELLID) || begin
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CELLID)
+      cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton
+      cell
+    end
+
+    beer = Beer::All[indexPath.row]
+    cell.textLabel.text = beer.title
+    return cell
+  end
+
+  def tableView(tableView, accessoryButtonTappedForRowWithIndexPath:indexPath)
+    beer = Beer::All[indexPath.row]
+    controller = UIApplication.sharedApplication.delegate.beer_details_controller
+    navigationController.pushViewController(controller, animated:true)
+    controller.showDetailsForBeer(beer)
+  end
+end
--- sample/mapview/app/main.rb
+++ sample/mapview/app/main.rb
@@ -1,43 +1,15 @@
-class Beer
-  def initialize(lat, long, name)
-    @name = name
-    @coordinate = CLLocationCoordinate2D.new
-    @coordinate.latitude = lat
-    @coordinate.longitude = long
-  end
-
-  def title; @name; end
-  def coordinate; @coordinate; end
-end
-
-class MapViewController < UIViewController
-  def loadView
-    self.view = MKMapView.alloc.init
-  end
-
-  def viewDidLoad 
-    # Center on Brussels.
-    region = MKCoordinateRegionMake(CLLocationCoordinate2D.new(50.85, 4.35), MKCoordinateSpanMake(3.1, 3.1))
-    self.view.setRegion(region)
-
-    beers = [
-      Beer.new(50.016667, 4.316667, 'Chimay'),
-      Beer.new(49.639722, 5.348889, 'Orval'),
-      Beer.new(50.178162, 5.219879, 'Rochefort'),
-      Beer.new(51.284720, 4.656670, 'Westmalle'),
-      Beer.new(50.895942, 2.721262, 'Westvleteren'),
-      Beer.new(51.298800, 5.488570, 'Achel')
-    ]
-    beers.each { |beer| self.view.addAnnotation(beer) }
-  end
-end
-
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    window.rootViewController = MapViewController.alloc.init
+    tabbar = UITabBarController.alloc.init
+    tabbar.viewControllers = [BeerMap.alloc.init, BeerList.alloc.init]
+    tabbar.selectedIndex = 0
+    @beer_details_controller = BeerDetailsController.alloc.init
+    window.rootViewController = UINavigationController.alloc.initWithRootViewController(tabbar)
     window.rootViewController.wantsFullScreenLayout = true
     window.makeKeyAndVisible
     return true
   end
+
+  attr_reader :beer_details_controller
 end
