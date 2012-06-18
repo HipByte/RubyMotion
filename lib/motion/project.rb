HipByte/RubyMotion

commit 01315ce30ce5e5e2f74618862a1172aca95b9cdf

add target option on :simulator task

lib/motion/project.rb
@@ -61,7 +61,7 @@ end
 desc "Run the simulator"
 task :simulator => ['build:simulator'] do
   app = App.config.app_bundle('iPhoneSimulator')
-  target = App.config.sdk_version
+  target = ENV['target'] || App.config.deployment_target
 
   # Cleanup the simulator application sandbox, to avoid having old resource files there.
   if ENV['clean']

commit b94b83be72bd2834e0f9e802ec4425474f5770d2

fix a regression in 'rake simulator' introduced by 01315ce30 - also, the 'deployment_target' env variable becomes 'target'

lib/motion/project.rb
@@ -61,7 +61,7 @@ end
 desc "Run the simulator"
 task :simulator => ['build:simulator'] do
   app = App.config.app_bundle('iPhoneSimulator')
-  target = ENV['deployment_target'] || App.config.deployment_target
+  target = ENV['target'] || App.config.sdk_version
 
   # Cleanup the simulator application sandbox, to avoid having old resource files there.
   if ENV['clean']