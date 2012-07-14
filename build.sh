6284288e1e56df65ab9275de767b7e3a9bfd3398
Laurent Sansonetti <lsansonetti@apple.com>
2012-07-14 13:15:53 +0200

    use DP2

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rake
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP1.app/Contents/Developer/Platforms rake
+SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP2.app/Contents/Developer/Platforms rake
