38d4190bbf584c7a289178d4e826d53105e31935
Laurent Sansonetti <lsansonetti@apple.com>
2012-07-28 22:37:42 +0200

    DP3

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rake
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP2.app/Contents/Developer/Platforms rake
+SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP3.app/Contents/Developer/Platforms rake
