4a1727db801fee74f210bff1806905643819f3c0
Laurent Sansonetti <lrz@hipbyte.com>
2012-09-11 15:37:36 +0200

    DP4

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rake
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP3.app/Contents/Developer/Platforms rake
+SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode45-DP4.app/Contents/Developer/Platforms rake
