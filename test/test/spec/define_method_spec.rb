2b7df372dbaf984631e41c6ef9dfb7155d9006d7
Laurent Sansonetti <lrz@hipbyte.com>
2012-09-26 14:40:56 +0200

    add more tests for define_method

---
+++ test/test/spec/define_method_spec.rb
@@ -0,0 +1,51 @@
+class TestDefineMethod
+  define_method :test1 { 42 }
+  define_method :test2 { |x| x + 100 }
+  define_method :test3 { |x, y| x + y }
+  define_method :test4 { |*ary| ary.inject(0) { |m, x| m + x } }     
+end
+
+describe "define_method" do
+  it "defines pure-Ruby methods" do
+    obj = TestDefineMethod.new
+    obj.test1.should == 42
+    obj.test2(42).should == 142
+    obj.test3(40, 2).should == 42
+    obj.test4(10, 10, 10, 10, 1, 1).should == 42
+  end
+end
+
+module TestDefineModuleExtendSelfBefore
+  define_method :foo { 42 }
+  extend self
+end
+
+describe "define_method" do
+  it "defines methods that are copied upon before module inclusion" do
+    TestDefineModuleExtendSelfBefore.foo.should == 42
+  end
+end
+
+module TestDefineModuleExtendSelfAfter
+  extend self
+  define_method :foo { 42 }
+end
+
+describe "define_method" do
+  it "defines methods that are copied upon after module inclusion" do
+    TestDefineModuleExtendSelfAfter.foo.should == 42
+  end
+end
+
+module TestDefineMethodAlias
+  define_method :test { 42 }
+  alias_method :test2, :test
+end
+
+describe "define_method" do
+  it "defines methods that are copied upon aliasing" do
+    obj = TestDefineMethodAlias.new
+    obj.test.should == 42
+    obj.test2.should == 42
+  end
+end
