9320db598e8c248623655829c768915db2bbdddd
Watson <watson1978@gmail.com>
2012-11-14 15:58:42 +0900

    install yard gem (v0.8.3)

--- Rakefile
+++ Rakefile
@@ -69,9 +69,11 @@ desc "Install"
 task :install do
   public_binaries = ['./bin/motion']
   binaries = public_binaries.dup.concat(['./bin/deploy', './bin/sim',
-    './bin/llc', './bin/ruby', './bin/ctags'])
+    './bin/llc', './bin/ruby', './bin/ctags',
+    'lib/yard/bin/yard', 'lib/yard/bin/yardoc', 'lib/yard/bin/yri'])
   data = ['./NEWS']
   data.concat(Dir.glob('./lib/**/*'))
+  data.delete_if { |x| true if x.include?("lib/yard/bin/") }
   SDK_VERSIONS.each do |sdk_version|
     data.concat(Dir.glob("./data/#{sdk_version}/BridgeSupport/*.bridgesupport"))
     data.concat(Dir.glob("./data/#{sdk_version}/iPhoneOS/*"))
---
+++ lib/yard/lib/yard/i18n/locale.rb
@@ -0,0 +1,58 @@
+module YARD
+  module I18n
+    # +Locale+ is a unit of translation. It has {#name} and a set of
+    # messages.
+    #
+    # @since 0.8.2
+    class Locale
+      # @return [String] the name of the locale. It used IETF language
+      #   tag format +[language[_territory][.codeset][@modifier]]+.
+      # @see http://tools.ietf.org/rfc/bcp/bcp47.txt
+      #   BCP 47 - Tags for Identifying Languages
+      attr_reader :name
+
+      # Creates a locale for +name+ locale.
+      #
+      # @param [String] name the locale name.
+      def initialize(name)
+        @name = name
+        @messages = {}
+      end
+
+      # Loads translation messages from +locale_directory+/{#name}.po.
+      #
+      # @param [String] locale_directory the directory path that has
+      #   {#name}.po.
+      # @return [Boolean] +true+ if PO file exists, +false+ otherwise.
+      def load(locale_directory)
+        return false if @name.nil?
+
+        po_file = File.join(locale_directory, "#{@name}.po")
+        return false unless File.exist?(po_file)
+
+        begin
+          require "gettext/tools/poparser"
+          require "gettext/runtime/mofile"
+        rescue LoadError
+          log.warn "Need gettext gem for i18n feature:"
+          log.warn "  gem install gettext"
+          return false
+        end
+
+        parser = GetText::PoParser.new
+        parser.report_warning = false
+        data = GetText::MoFile.new
+        parser.parse_file(po_file, data)
+        @messages.merge!(data)
+        true
+      end
+
+      # @param [String] message the translation target message.
+      # @return [String] translated message. If tarnslation isn't
+      #   registered, the +message+ is returned.
+      def translate(message)
+        @messages[message] || message
+      end
+    end
+  end
+end
