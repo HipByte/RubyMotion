72f664f72d6c348b2c011c7438c2a348c54711e4
Laurent Sansonetti <lrz@hipbyte.com>
2012-12-20 15:53:00 +0530

    work around a compiler problem (time method signature)

--- lib/motion/spec.rb
+++ lib/motion/spec.rb
@@ -30,29 +30,29 @@ module Bacon
 
     def handle_specification_begin(name)
       unless @@entered
-        puts "##teamcity[enteredTheMatrix timestamp = '#{time}']\n\n"
+        puts "##teamcity[enteredTheMatrix timestamp = '#{java_time}']\n\n"
         @@entered = true
       end
       @@specification = name
-      puts "##teamcity[testSuiteStarted timestamp = '#{time}' name = '#{escape_message(name)}']\n\n"
+      puts "##teamcity[testSuiteStarted timestamp = '#{java_time}' name = '#{escape_message(name)}']\n\n"
     end
 
     def handle_specification_end
-      puts "##teamcity[testSuiteFinished timestamp = '#{time}' name = '#{escape_message(@@specification)}']\n\n" if Counter[:context_depth] == 1
+      puts "##teamcity[testSuiteFinished timestamp = '#{java_time}' name = '#{escape_message(@@specification)}']\n\n" if Counter[:context_depth] == 1
     end
 
     def handle_requirement_begin(description)
       @@description = description
       @@started = Time.now
-      puts "##teamcity[testStarted timestamp = '#{time}' captureStandardOutput = 'true' name = '#{escape_message(description)}']\n\n"
+      puts "##teamcity[testStarted timestamp = '#{java_time}' captureStandardOutput = 'true' name = '#{escape_message(description)}']\n\n"
     end
 
     def handle_requirement_end(error)
       if !error.empty?
-        puts "##teamcity[testFailed timestamp = '#{time}' message = '#{escape_message(error)}' name = '#{escape_message(@@description)}']\n\n"
+        puts "##teamcity[testFailed timestamp = '#{java_time}' message = '#{escape_message(error)}' name = '#{escape_message(@@description)}']\n\n"
       end
       duration = ((Time.now - @@started) * 1000).to_i
-      puts "##teamcity[testFinished timestamp = '#{time}' duration = '#{duration}' name = '#{escape_message(@@description)}']\n\n"     
+      puts "##teamcity[testFinished timestamp = '#{java_time}' duration = '#{duration}' name = '#{escape_message(@@description)}']\n\n"     
     end
 
     def handle_summary
@@ -65,7 +65,7 @@ module Bacon
       "  " * (Counter[:context_depth] - 1)
     end
 
-    def time
+    def java_time
       convert_time_to_java_simple_date(Time.now)
     end
 
