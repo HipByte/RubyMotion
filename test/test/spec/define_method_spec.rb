ddb65efb3da27f51fb7872359c1bf1272d62ee23
Watson <watson1978@gmail.com>
2013-03-21 16:51:48 +0900

    update define_method_spec.rb

--- test/test/spec/define_method_spec.rb
+++ test/test/spec/define_method_spec.rb
@@ -52,6 +52,7 @@ end
 
 class TestIncludedModule
   include TestDefineMethodAlias
+  include TestDefineModuleExtendSelfBefore
 end
 
 describe "define_method" do
@@ -60,5 +61,6 @@ describe "define_method" do
     obj = TestIncludedModule.new
     obj.test.should == 42
     obj.test2.should == 42
+    obj.foo.should == 42
   end
 end
