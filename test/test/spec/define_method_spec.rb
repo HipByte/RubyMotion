1755d2b69a5a2cb6cbe0ea4ed22b35b74104f867
Watson <watson1978@gmail.com>
2013-03-21 15:47:53 +0900

    add spec to relate RM-37

--- test/test/spec/define_method_spec.rb
+++ test/test/spec/define_method_spec.rb
@@ -49,3 +49,16 @@ describe "define_method" do
     obj.test2.should == 42
   end
 end
+
+class TestIncludedModule
+  include TestDefineMethodAlias
+end
+
+describe "define_method" do
+  # RM-37 Assertion failed when trying to define a method on a module
+  it "should work on class which included module" do
+    obj = TestIncludedModule.new
+    obj.test.should == 42
+    obj.test2.should == 42
+  end
+end
