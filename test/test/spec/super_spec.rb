a6ac6231bcc829ce7a56ec3830046e24c2a9c3c8
Watson <watson1978@gmail.com>
2013-04-04 21:38:02 +0900

    modify spec description

--- test/test/spec/super_spec.rb
+++ test/test/spec/super_spec.rb
@@ -18,7 +18,7 @@ end
 $spec_result = []
 
 describe "'super'" do
-  it "call inherited class method" do
+  it "should work when calls inherited class method in super method" do
     obj = Foo.new
     obj.bar(%w{1 2 3})
     $spec_result.should == ["a", "1", "a", "2", "a", "3", "a", nil]
