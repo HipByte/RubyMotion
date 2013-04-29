e4ebfcdb4cf18d217555109b961ee4be9923156a
Watson <watson1978@gmail.com>
2013-04-30 08:36:43 +0900

    delete trailing whitespaces

--- lib/motion/project/template/osx/files/app/app_delegate.rb
+++ lib/motion/project/template/osx/files/app/app_delegate.rb
@@ -6,8 +6,8 @@ class AppDelegate
 
   def buildWindow
     @mainWindow = NSWindow.alloc.initWithContentRect([[240, 180], [480, 360]],
-      styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask, 
-      backing: NSBackingStoreBuffered, 
+      styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
+      backing: NSBackingStoreBuffered,
       defer: false)
     @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
     @mainWindow.orderFrontRegardless
