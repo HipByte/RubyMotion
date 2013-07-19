HipByte/RubyMotion

commit 3847d29e318fc0baf497c7b87a2f71068ba94bf8

use lldb if gdb is not present, honor malloc_debug env variable (1 for malloc history debug, 2 for guard malloc)

bin/sim.m
@@ -965,12 +965,25 @@ gdb_commands_file(void)
 	// Forward ^C to gdb.
 	signal(SIGINT, sigforwarder);
 
-	// Run the gdb process.
+	// Run the debugger process.
 	NSString *gdb_path = [xcode_path stringByAppendingPathComponent:@"Platforms/iPhoneSimulator.platform/Developer/usr/libexec/gdb/gdb-i386-apple-darwin"];
-	gdb_task = [[NSTask launchedTaskWithLaunchPath:gdb_path
-	    arguments:[NSArray arrayWithObjects:@"--arch", @"i386", @"-q",
-	    @"--pid", [pidNumber description], @"-x", gdb_commands_file(),
-	    nil]] retain];
+	NSString *lldb_path = [xcode_path stringByAppendingPathComponent:@"Platforms/iPhoneSimulator.platform/Developer/usr/bin/lldb"];
+	if ([[NSFileManager defaultManager] fileExistsAtPath:gdb_path]) {
+	    gdb_task = [[NSTask launchedTaskWithLaunchPath:gdb_path
+		arguments:[NSArray arrayWithObjects:@"--arch", @"i386", @"-q",
+		@"--pid", [pidNumber description], nil]] retain];
+	}
+	else if ([[NSFileManager defaultManager] fileExistsAtPath:lldb_path]) {
+	    gdb_task = [[NSTask launchedTaskWithLaunchPath:lldb_path
+		arguments:[NSArray arrayWithObjects:@"-a", @"i386",
+		@"-p", [pidNumber description], @"-x", gdb_commands_file(),
+		nil]] retain];
+	}
+	else {
+	    fprintf(stderr, "can't locate a debugger (gdb `%s' or lldb `%s')\n",
+		    [gdb_path UTF8String], [lldb_path UTF8String]);
+	    exit(1);
+	}
 	[gdb_task waitUntilExit];
 	gdb_task = nil;
 
@@ -1051,7 +1064,7 @@ main(int argc, char **argv)
     // Prepare app environment.
     NSMutableDictionary *appEnvironment = [[[NSProcessInfo processInfo]
 	environment] mutableCopy];
-    if (debug_mode == DEBUG_REPL) {
+    if (debug_mode != DEBUG_NOTHING) {
 	// Prepare repl socket path.
 	NSString *tmpdir = NSTemporaryDirectory();
 	assert(tmpdir != nil);
@@ -1091,7 +1104,17 @@ main(int argc, char **argv)
 	[appEnvironment setObject:replPath forKey:@"REPL_DYLIB_PATH"];
     }
 
-    //[NSDictionary dictionaryWithObjectsAndKeys:@"/usr/lib/libgmalloc.dylib", @"DYLD_INSERT_LIBRARIES", nil]);
+    char *malloc_debug_level = NULL;
+    if ((malloc_debug_level = getenv("malloc_debug")) != NULL) {
+	int level = atoi(malloc_debug_level);
+	if (level >= 1) {
+	    [appEnvironment setObject:@"1" forKey:@"MallocStackLoggingNoCompact"];
+	}
+        if (level >= 2) {
+	    [appEnvironment setObject:@"/usr/lib/libgmalloc.dylib"
+		forKey:@"DYLD_INSERT_LIBRARIES"];
+	}
+    }
 
 #if defined(SIMULATOR_IOS)
     // Create application specifier.

commit 8384e336f9668d46bb35cd66fef8390bac53b321

fix a bug in debugger where built-in debug commands are not working
http://hipbyte.myjetbrains.com/youtrack/issue/RM-187

This bug was introduced by https://github.com/lrz/RubyMotion/commit/3847d29e318fc0baf497c7b87a2f71068ba94bf8

bin/sim.m
@@ -982,7 +982,7 @@ gdb_commands_file(void)
 	if ([[NSFileManager defaultManager] fileExistsAtPath:gdb_path]) {
 	    gdb_task = [[NSTask launchedTaskWithLaunchPath:gdb_path
 		arguments:[NSArray arrayWithObjects:@"--arch", @"i386", @"-q",
-		@"--pid", [pidNumber description], nil]] retain];
+		@"--pid", [pidNumber description], @"-x", gdb_commands_file(), nil]] retain];
 	}
 	else if ([[NSFileManager defaultManager] fileExistsAtPath:lldb_path]) {
 	    gdb_task = [[NSTask launchedTaskWithLaunchPath:lldb_path