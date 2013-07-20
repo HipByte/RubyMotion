HipByte/RubyMotion

commit 2617e9a3e4a0d99041cad8631ea8b0ad72022ade

fix warning message where it would display "ld: warning: ObjC object file (./build/iPhoneSimulator-7.0-Development/objs/init.o) was compiled for iOS Simulator, but linking for MacOSX"
use "-mios-simulator-version-min" instead of "-miphoneos-version-min"

lib/motion/project/template/ios/config.rb
@@ -136,7 +136,17 @@ module Motion; module Project;
     end
 
     def common_flags(platform)
-      super + " -miphoneos-version-min=#{deployment_target}"
+      simulator_version = begin
+        if platform == "iPhoneSimulator"
+          m = deployment_target.match(/(\d+)/)
+          if m[0].to_i >= 7
+            " -mios-simulator-version-min=#{deployment_target}"
+          else
+            " -miphoneos-version-min=#{deployment_target}"
+          end
+        end
+      end || ""
+      super + simulator_version
     end
 
     def cflags(platform, cplusplus)

commit 3b75fb79eee5c221e6e28a83a718ee82d920d0b3

fix a bug in build system where app does not work on iOS5 device
http://hipbyte.myjetbrains.com/youtrack/issue/RM-221

The compile flags for device are missing.
This bug was introduced by 2617e9a3e4a0d99041cad8631ea8b0ad72022ade

lib/motion/project/template/ios/config.rb
@@ -137,15 +137,15 @@ module Motion; module Project;
 
     def common_flags(platform)
       simulator_version = begin
+        flag = " -miphoneos-version-min=#{deployment_target}"
         if platform == "iPhoneSimulator"
           ver = xcode_version[0].match(/(\d+)/)
           if ver[0].to_i >= 5
-            " -mios-simulator-version-min=#{deployment_target}"
-          else
-            " -miphoneos-version-min=#{deployment_target}"
+            flag = " -mios-simulator-version-min=#{deployment_target}"
           end
         end
-      end || ""
+        flag
+      end
       super + simulator_version
     end
 