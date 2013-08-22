27b5a32a1247ace39b28c9236c4677b48a49f6c1
Watson <watson1978@gmail.com>
2013-08-22 13:06:49 +0900

    build with Xcode5 DP6

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rake
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode5-DP5.app/Contents/Developer/Platforms rake
+SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode5-DP6.app/Contents/Developer/Platforms rake
