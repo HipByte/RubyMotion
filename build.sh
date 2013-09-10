61d98efed1d0012c8e5af84f039f391d550a72a9
Watson <watson1978@gmail.com>
2013-09-10 09:33:27 +0900

    accept the arguments in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-rake
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode5-DP6.app/Contents/Developer/Platforms rake
+#!/bin/sh
+
+# EX)
+#   ./build.sh clean:vm
+#   ./build.sh
+rake $*
+SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode5-DP6.app/Contents/Developer/Platforms rake $*
