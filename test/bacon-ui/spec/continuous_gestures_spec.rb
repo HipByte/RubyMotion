af886e9a9eb66d2cac727fdc4111ff05f85c978b
Eloy Durán <eloy.de.enige@gmail.com>
2013-10-02 14:24:57 +0200

    Import Bacon UI helper tests from public toolchain repo.

---
+++ test/bacon-ui/spec/continuous_gestures_spec.rb
@@ -0,0 +1,96 @@
+class ScrollViewController < UIViewController
+  attr_reader :scrollView, :scrollViewRotation, :imageView
+
+  def loadView
+    @scrollViewRotation = 0
+
+    frame = UIScreen.mainScreen.applicationFrame
+    frame.origin = CGPointZero
+    self.view = UIView.alloc.initWithFrame(frame)
+
+    @scrollView = UIScrollView.alloc.initWithFrame(view.bounds)
+    @scrollView.backgroundColor = UIColor.redColor
+    @scrollView.delegate = self
+    @scrollView.maximumZoomScale = 3
+    view.addSubview(@scrollView)
+
+    @imageView = UIImageView.alloc.initWithImage(UIImage.imageNamed('We Need You.jpg'))
+    @imageView.frame = [[0, 0], @imageView.image.size]
+    @scrollView.accessibilityLabel = 'Scroll view'
+
+    @scrollView.contentSize = @imageView.image.size
+    @scrollView.addSubview(@imageView)
+
+    recognizer = UIRotationGestureRecognizer.alloc.initWithTarget(self, action:'handleRotation:')
+    @scrollView.addGestureRecognizer(recognizer)
+  end
+
+  def viewForZoomingInScrollView(sv)
+    @imageView
+  end
+
+  def handleRotation(recognizer)
+    @scrollViewRotation = recognizer.rotation
+    @scrollView.transform = CGAffineTransformMakeRotation(recognizer.rotation)
+  end
+end
+
+describe "Bacon::Functional::API, concerning continuous gestures" do
+  tests ScrollViewController
+
+  # TODO this spec fails if the Accessibility Inspector is on
+  it "creates 'pinch open' and 'pinch close' gesture events" do
+    before = controller.scrollView.zoomScale
+    pinch_open 'Scroll view'
+    controller.scrollView.zoomScale.should > before
+
+    before = controller.scrollView.zoomScale
+    pinch_close 'Scroll view'
+    controller.scrollView.zoomScale.should < before
+  end
+
+  # TODO currently it doesn't rotate exactly 90 degreese. I think this has to
+  # do with the center point being off and the radius not being a perfect half
+  # of the total diameter.
+  it "creates rotate gesture events" do
+    before = controller.scrollViewRotation
+    rotate 'Scroll view', :degrees => 90
+    controller.scrollViewRotation.should > before
+
+    #before = controller.scrollView.transform
+    #rotate 'Scroll view', :degrees => 90, :duration => 5
+    #after = controller.scrollView.transform
+    #expected = CGAffineTransformRotate(before, Math::PI/2)
+    #CGAffineTransformEqualToTransform(after, expected).should == true
+  end
+
+  before do
+    pinch_open 'Scroll view'
+  end
+
+  # TODO this spec fails if the Accessibility Inspector is on
+  it "drags from point A to point B" do
+    before = controller.scrollView.contentOffset
+    drag 'Scroll view', :from => CGPointMake(310, 100), :to => CGPointMake(5, 150)
+    controller.scrollView.contentOffset.x.should > before.x
+    controller.scrollView.contentOffset.y.should < before.y
+  end
+
+  # TODO this spec fails if the Accessibility Inspector is on
+  it "drags along the specified list of points" do
+    view = controller.scrollView
+    before = view.contentOffset
+    drag 'Scroll view', :points => linear_interpolate(_location_to_point(view, :bottom_right), _location_to_point(view, :left))
+    view.contentOffset.x.should > before.x
+    view.contentOffset.y.should > before.y
+  end
+
+  it "drags with multiple fingers" do
+    controller.scrollView.panGestureRecognizer.minimumNumberOfTouches = 3
+    controller.scrollView.panGestureRecognizer.maximumNumberOfTouches = 3
+    before = controller.scrollView.contentOffset
+    drag 'Scroll view', :from => :right, :touches => 3
+    controller.scrollView.contentOffset.x.should >  before.x
+    controller.scrollView.contentOffset.y.should == before.y
+  end
+end
---
+++ test/bacon-ui/spec/device_events_spec.rb
@@ -0,0 +1,99 @@
+class DeviceSpecController < UIViewController
+  def loadView
+    frame = UIScreen.mainScreen.applicationFrame
+    frame.origin = CGPointZero
+    self.view = UIImageView.alloc.initWithFrame(frame)
+    view.image = UIImage.imageNamed('We Need You.jpg')
+  end
+
+  def shouldAutorotateToInterfaceOrientation(orientation)
+    true
+  end
+
+  # This is all for `shake` support
+
+  attr_accessor :shaked
+  def shaked?
+    @shaked
+  end
+
+  def viewWillAppear(animated)
+    super
+    becomeFirstResponder
+  end
+
+  def viewDidDisappear(animated)
+    super
+    resignFirstResponder
+  end
+
+  def canBecomeFirstResponder
+    true
+  end
+
+  def motionEnded(motion, withEvent:event)
+    @shaked = motion == UIEventSubtypeMotionShake
+  end
+
+  # Accelerometer support
+
+  def enableAccelerometer=(flag)
+    UIAccelerometer.sharedAccelerometer.delegate = (flag == true ? self : nil)
+  end
+
+  attr_reader :accelerationData
+  def accelerometer(accelerometer, didAccelerate:acceleration)
+    @accelerationData = acceleration
+  end
+end
+
+describe "Bacon::Functional::API, concerning device events" do
+  tests DeviceSpecController
+
+  after do
+    rotate_device :to => :portrait
+  end
+
+  it "changes device orientation" do
+    rotate_device :to => :landscape, :button => :right
+    controller.interfaceOrientation.should == UIInterfaceOrientationLandscapeRight
+
+    rotate_device :to => :landscape, :button => :left
+    controller.interfaceOrientation.should == UIInterfaceOrientationLandscapeLeft
+
+    rotate_device :to => :portrait,  :button => :bottom
+    controller.interfaceOrientation.should == UIInterfaceOrientationPortrait
+
+    rotate_device :to => :portrait,  :button => :top
+    controller.interfaceOrientation.should == UIInterfaceOrientationPortraitUpsideDown
+  end
+
+  it "has default orientations for portrait and landscape for when the :button option is omitted" do
+    rotate_device :to => :landscape
+    controller.interfaceOrientation.should == UIInterfaceOrientationLandscapeLeft
+
+    rotate_device :to => :portrait
+    controller.interfaceOrientation.should == UIInterfaceOrientationPortrait
+  end
+
+  it "creates a shake motion gesture (for undo support, for instance)" do
+    shake
+    controller.should.be.shaked
+  end
+
+  it "sends accelerometer events" do
+    with_accelerometer do
+      accelerate :x => 0.5, :y => 0.5, :z => 0.5
+    end
+    controller.accelerationData.x.should == 0.5
+    controller.accelerationData.y.should == 0.5
+    controller.accelerationData.z.should == 0.5
+  end
+
+  def with_accelerometer
+    controller.enableAccelerometer = true
+    yield
+  ensure
+    controller.enableAccelerometer = false
+  end
+end
