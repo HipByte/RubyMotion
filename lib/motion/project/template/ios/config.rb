fe3b93d464a702400b1d4890214c9eb5ee1f919e
Watson <watson1978@gmail.com>
2013-10-07 11:10:20 +0900

    [iOS] add support to launch the app as 64-bit in simulator
    When specified "app.archs['iPhoneSimulator'] << 'x86_64'" and use iOS 7 retina 4 simulator,
    we can launch 64-bit simulator.

--- lib/motion/project/template/ios/config.rb
+++ lib/motion/project/template/ios/config.rb
@@ -205,9 +205,17 @@ module Motion; module Project;
     end
 
     def device_retina_xcode5_string(family, target, retina)
+      retina4_string = begin
+        if (target >= '7.0' && App.config.archs['iPhoneSimulator'].include?("x86_64")) 
+          " Retina (4-inch 64-bit)"
+        else
+          " Retina (4-inch)"
+        end
+      end
+
       case retina
       when 'true'
-        (family == 1 and target >= '6.0') ? ' Retina (4-inch)' : ' Retina'
+        (family == 1 and target >= '6.0') ? retina4_string : ' Retina'
       when '3.5'
         ' Retina (3.5-inch)'
       when '4'
@@ -216,7 +224,7 @@ module Motion; module Project;
         if target < '7.0'
           ''
         else
-          (family == 1) ? ' Retina (4-inch)' : ' Retina'
+          (family == 1) ? retina4_string : ' Retina'
         end
       end
     end
