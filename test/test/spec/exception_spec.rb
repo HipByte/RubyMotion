fe3d89436d997fdd6429d9561753d381e2215e5f
Laurent Sansonetti <lrz@hipbyte.com>
2013-10-08 17:12:33 +0200

    add another test for NSException catching

--- test/test/spec/exception_spec.rb
+++ test/test/spec/exception_spec.rb
@@ -1,5 +1,5 @@
 describe "NSExceptions" do
-  it "can be catched" do
+  it "can be caught (1)" do
     exc_name = 'TestException'
     exc_reason = 'some reason'
     exc_userInfo = { 'One' => 1, 'Two' => 2, 'Three' => 3}
@@ -9,6 +9,15 @@ describe "NSExceptions" do
       e.name.should == exc_name
       e.reason.should == exc_reason
       e.userInfo.should == exc_userInfo
+    end
+  end
+
+  it "can be caught (2)" do
+    begin
+      NSString.stringWithString(42)
+    rescue => e
+      e.class.should == NSException
+      e.name.should == 'NSInvalidArgumentException'
     end
   end
 end
