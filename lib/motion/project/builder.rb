1c4953f026b84cd720eab6dddadec82493bfb847
Eloy Durán <eloy.de.enige@gmail.com>
2013-10-14 15:59:24 +0200

    [xcassets] All files that are to be preserved are in `preserve_resources`.

--- lib/motion/project/builder.rb
+++ lib/motion/project/builder.rb
@@ -423,7 +423,7 @@ EOS
       FileUtils.mkdir_p(app_resources_dir)
       reserved_app_bundle_files = [
         '_CodeSignature/CodeResources', 'CodeResources', 'embedded.mobileprovision',
-        'Info.plist', 'PkgInfo', 'ResourceRules.plist', 'Assets.car',
+        'Info.plist', 'PkgInfo', 'ResourceRules.plist',
         convert_filesystem_encoding(config.name)
       ]
       resources_exclude_extnames = ['.xib', '.storyboard', '.xcdatamodeld', '.lproj', '.atlas', '.xcassets']
