28423f9e4f8856faab6b79a1485a2d0e38997f46
Eloy Durán <eloy.de.enige@gmail.com>
2013-10-25 14:02:01 +0200

    [spec] Comment crashing CoreMIDI spec so we can use the run-disabled env var.

--- test/test/spec/coremidi_spec.rb
+++ test/test/spec/coremidi_spec.rb
@@ -1,10 +1,11 @@
 describe "CoreMidi" do
   xit "can list devices" do
-    n = MIDIGetNumberOfDevices()
-    n.should > 0
-    n.times do |i|
-      device = MIDIGetDevice(i)
-      device.class.should == MIDIDeviceRef
-    end
+    # This actually crashes.
+    #n = MIDIGetNumberOfDevices()
+    #n.should > 0
+    #n.times do |i|
+      #device = MIDIGetDevice(i)
+      #device.class.should == MIDIDeviceRef
+    #end
   end
 end
