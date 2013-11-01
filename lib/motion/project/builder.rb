cf145c8df4fd95064b57af3c95ace18247598006
Laurent Sansonetti <lrz@hipbyte.com>
2013-11-01 17:50:15 +0100

    add a hidden variable to force symbol strip (ex. rake device __strip__=1)

--- lib/motion/project/builder.rb
+++ lib/motion/project/builder.rb
@@ -483,7 +483,7 @@ EOS
       end
 
       # Strip all symbols. Only in distribution mode.
-      if main_exec_created and config.distribution_mode
+      if main_exec_created and (config.distribution_mode or ENV['__strip__'])
         App.info "Strip", main_exec
         sh "#{config.locate_binary('strip')} #{config.strip_args} \"#{main_exec}\""
       end
