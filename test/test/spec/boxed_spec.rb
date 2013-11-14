db916707c52f38dbf5d1735a841422766b727793
Watson <watson1978@gmail.com>
2013-11-14 10:48:56 +0900

    add spec for Boxed
    Related to https://github.com/lrz/RubyMotionVM/commit/9d8885ec3968a441d417acb1e7cff2a0fb33e679

--- test/test/spec/boxed_spec.rb
+++ test/test/spec/boxed_spec.rb
@@ -7,4 +7,42 @@ describe "Boxed" do
     rect = CGRect.new(CGPoint.new(1, 2), CGSize.new(3, 4))
     rect.to_a.should == [[1.0, 2.0], [3.0, 4.0]]
   end
+
+  it "#==" do
+    CGPoint.new(1, 2).should == CGPoint.new(1, 2)
+  end
+
+  it "#inspect" do
+    CGPoint.new(1, 2).inspect.should == "#<CGPoint x=1.0 y=2.0>"
+  end
+
+  it "#to_a" do
+    CGPoint.new(1, 2).to_a.should == [1.0, 2.0]
+  end
+
+  it "#[]" do
+    point = CGPoint.new(1, 2)
+    point[0].should == 1.0
+    point[1].should == 2.0
+  end
+
+  it "#[]=" do
+    point = CGPoint.new(1, 2)
+    point[0] = 42
+    point[0].should == 42
+  end
+
+  it "#dup" do
+    point = CGPoint.new(1, 2)
+    point2 = point.dup
+    point.should == point2
+  end
+
+  it ".fields" do
+    CGPoint.fields.should == [:x, :y]
+  end
+
+  it ".opaque?" do
+    CGPoint.opaque?.should == false
+  end
 end
