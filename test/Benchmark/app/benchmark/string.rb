05f450dc9f9163a9ba7093a659e44c2263aeb734
Watson <watson1978@gmail.com>
2013-11-19 00:12:13 +0900

    fix a benchmark for ASCII string where UTF8 character is contained

--- test/Benchmark/app/benchmark/string.rb
+++ test/Benchmark/app/benchmark/string.rb
@@ -24,7 +24,7 @@ surgeons in the Army. Having completed my studies there, I was duly attached
 to the Fifth Northumberland Fusiliers as assistant surgeon. The regiment was
 stationed in India at the time, and before I could join it, the second Afghan
 war had broken out. On landing at Bombay, I learned that my corps had advanced
-through the passes, and was already deep in the enemy’s country. I followed,
+through the passes, and was already deep in the enemy's country. I followed,
 however, with many other officers who were in the same situation as myself, and
 succeeded in reaching Candahar in safety, where I found my regiment, and at
 once entered upon my new duties. 
