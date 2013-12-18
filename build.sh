b5150ec05c355eb880f7fd056e6df08470549713
Watson <watson1978@gmail.com>
2013-12-19 00:30:57 +0900

    PLATFORMS_DIR -> XCODE_PLATFORMS_DIR

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   ./build.sh clean:vm
 #   ./build.sh
 rake $*
-SDK_BETA=1 PLATFORMS_DIR=/Applications/Xcode51-DP2.app/Contents/Developer/Platforms rake $*
+SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode51-DP2.app/Contents/Developer/Platforms rake $*
