55f1322e4ecf6173e855abb33eaaeaf9bba99fa4
Eloy Durán <eloy.de.enige@gmail.com>
2014-01-16 11:19:01 +0100

    [vendor] Default to `:cflags` option for BS generation.
    Fixes http://hipbyte.myjetbrains.com/youtrack/issue/RM-388.

--- lib/motion/project/vendor.rb
+++ lib/motion/project/vendor.rb
@@ -133,7 +133,7 @@ EOS
         unless headers.empty?
           bs_file = File.basename(@path) + '.bridgesupport'
           if !File.exist?(bs_file) or headers.any? { |h| File.mtime(h) > File.mtime(bs_file) }
-            bs_cflags = (opts.delete(:bridgesupport_cflags) or '')
+            bs_cflags = (opts.delete(:bridgesupport_cflags) or opts.delete(:cflags) or '')
             bs_exceptions = (opts.delete(:bridgesupport_exceptions) or [])
             @config.gen_bridge_metadata(platform, headers, bs_file, bs_cflags, bs_exceptions)
           end
@@ -203,7 +203,7 @@ EOS
           project_dir = File.expand_path(@config.project_dir)
           headers = Dir.glob(File.join(project_dir, headers_dir, '**/*.h'))
           if !File.exist?(bs_file) or headers.any? { |x| File.mtime(x) > File.mtime(bs_file) }
-            bs_cflags = (opts.delete(:bridgesupport_cflags) or '')
+            bs_cflags = (opts.delete(:bridgesupport_cflags) or opts.delete(:cflags) or '')
             bs_exceptions = (opts.delete(:bridgesupport_exceptions) or [])
             @config.gen_bridge_metadata(platform, headers, bs_file, bs_cflags, bs_exceptions)
           end
