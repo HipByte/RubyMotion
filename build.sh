c2e7c44fa0230c613abf90425a724731150b66ac
Watson <watson1978@gmail.com>
2014-01-21 09:11:34 +0900

    update for Xcode 5.1 beta 4

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   ./build.sh clean:vm
 #   ./build.sh
 rake $*
-SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode51-Beta3.app/Contents/Developer/Platforms rake $*
+SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode51-Beta4.app/Contents/Developer/Platforms rake $*
