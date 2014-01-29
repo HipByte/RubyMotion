d9accd8b834cb51f9d6b74bdeded183833c03fe8
Watson <watson1978@gmail.com>
2014-01-29 11:42:00 +0900

    fix a bug which `motion update --cache-version=X' is broken
    http://hipbyte.myjetbrains.com/youtrack/issue/RM-408

--- lib/motion/command/update.rb
+++ lib/motion/command/update.rb
@@ -106,7 +106,7 @@ module Motion; class Command
       need_root
 
       $stderr.puts "Connecting to the server..."
-      resp = curl("-s -d \"product=rubymotion\" -d \"current_software_version=#{product_version}\" -d \"@wanted_software_version=#{@wanted_software_version}\" -d \"license_key=#{read_license_key}\" https://secure.rubymotion.com/update_software")
+      resp = curl("-s -d \"product=rubymotion\" -d \"current_software_version=#{product_version}\" -d \"wanted_software_version=#{@wanted_software_version}\" -d \"license_key=#{read_license_key}\" https://secure.rubymotion.com/update_software")
       unless resp.match(/^http:/)
         die resp
       end
