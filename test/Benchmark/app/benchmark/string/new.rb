81cad2dcb44b95b6f432a6dfcd0779563852d8cb
Watson <watson1978@gmail.com>
2014-02-13 13:08:14 +0900

    [Benchmark] add String literal case

--- test/Benchmark/app/benchmark/string/new.rb
+++ test/Benchmark/app/benchmark/string/new.rb
@@ -3,6 +3,18 @@ def string_new(x)
   ascii = "The quick brown fox jumps over the lazy dog."
   utf8  = "飛べねぇ豚はタダの豚だ...................."
 
+  x.report "literal with ASCII" do
+    1000000.times do
+      "The quick brown fox jumps over the lazy dog."
+    end
+  end
+
+  x.report "literal with UTF8" do
+    1000000.times do
+      "飛べねぇ豚はタダの豚だ...................."
+    end
+  end
+
   x.report "new with ASCII" do
     100000.times do
       String.new(ascii)
