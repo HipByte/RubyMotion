bfabd1b92f65eb67b1adc4293088e720e68084c2
Watson <watson1978@gmail.com>
2014-02-20 13:10:50 +0900

    [spec] add test for RM-427

--- test/TestTime/spec/nsdate_spec.rb
+++ test/TestTime/spec/nsdate_spec.rb
@@ -11,5 +11,15 @@ describe "NSDate" do
     date = NSDate.alloc.initWithTimeIntervalSinceReferenceDate(-5978307200)
     date.should == Time.at(-5000000000)
   end
+
+  it ".distantFuture should return future time" do
+    date = NSDate.distantFuture
+    date.utc.to_i.should == (63113904000 + 978307200)
+  end
+
+  it ".distantPast should return past time" do
+    date = NSDate.distantPast
+    date.utc.to_i.should == (-63114076800 + 978307200)
+  end
 end
 
