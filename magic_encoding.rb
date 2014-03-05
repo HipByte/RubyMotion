a64576be7b7494c530af09e51e91bc01d88b32bf
Laurent Sansonetti <lrz@hipbyte.com>
2014-03-05 12:20:57 +0100

    forgot to add this file

---
+++ magic_encoding.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+require 'find'
+
+class MagicEncoding
+  MAGIC_ENCODING = "# encoding: utf-8\n\n"
+  SOURCE_FILE_RE = /\.(rb|rake)$/
+
+  def self.root
+    File.expand_path('lib')
+  end
+
+  def self.fix(content)
+    MAGIC_ENCODING + content.lstrip
+  end
+
+  def self.change?(filename)
+    File.open(filename) do |file|
+      if file.read(10) != '# encoding'
+        file.seek(0)
+        file.read
+      end
+    end
+  end
+
+  def self.files(count)
+    "#{count} #{count == 1 ? 'file' : 'files'}"
+  end
+
+  def self.apply
+    ignored = 0
+    fixed = 0
+
+    Find.find(root) do |filename|
+      if File.file?(filename)
+        if filename =~ SOURCE_FILE_RE
+          if content = change?(filename)
+            File.open(filename, 'w') do |file|
+              file.write(fix(content))
+            end
+            fixed += 1
+          else
+            ignored += 1
+          end
+        end
+      end
+    end
+
+    puts "Fixed #{files(fixed)}, #{files(ignored)} were already fine."
+  end
+end
