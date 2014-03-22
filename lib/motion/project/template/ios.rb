HipByte/RubyMotion

commit 064672443dc1ff3d8920023f51e46d73f90bbec1

[iOS] Fail build if one of the resources is a dir called `Resources'.
Fixes http://hipbyte.myjetbrains.com/youtrack/issue/RM-450.

lib/motion/project/template/ios.rb
@@ -40,21 +40,36 @@ desc "Build everything"
 task :build => ['build:simulator', 'build:device']
 
 namespace :build do
-  desc "Build the simulator version"
-  task :simulator do
+  def pre_build_actions(platform)
     # TODO: Ensure Info.plist gets regenerated on each build so it has ints for
     # Instruments and strings for normal builds.
-    rm_f File.join(App.config.app_bundle('iPhoneSimulator'), 'Info.plist')
+    rm_f File.join(App.config.app_bundle(platform), 'Info.plist')
 
+    # TODO this should go into a iOS specific Builder class which performs this
+    # check before building.
+    App.config.resources_dirs.each do |dir|
+      Dir.entries(dir).grep(/Resources/i).each do |basename|
+        path = File.join(dir, basename)
+        if File.directory?(path)
+          suggestion = basename == 'Resources' ? 'Assets' : 'assets'
+          App.fail "An iOS application cannot be installed if it contains a " \
+                   "directory called `resources'. Please rename the " \
+                   "directory at path `#{path}' to, for instance, " \
+                   "`#{File.join(dir, suggestion)}'."
+        end
+      end
+    end
+  end
+
+  desc "Build the simulator version"
+  task :simulator do
+    pre_build_actions('iPhoneSimulator')
     App.build('iPhoneSimulator')
   end
 
   desc "Build the device version"
   task :device do
-    # TODO: Ensure Info.plist gets regenerated on each build so it has ints for
-    # Instruments and strings for normal builds.
-    rm_f File.join(App.config.app_bundle('iPhoneOS'), 'Info.plist')
-
+    pre_build_actions('iPhoneOS')
     App.build('iPhoneOS')
     App.codesign('iPhoneOS')
   end

commit a4cd4a4cecc3263a5b9d01a959a6828f30cf1040

fix build error if resource directory does not exits
This bug is introduced by 064672443dc1ff3d8920023f51e46d73f90bbec1

% motion create hello && cd hello
% rm -rf resources
% rake
rake aborted!
No such file or directory @ dir_initialize - ./resources
/Library/RubyMotion/lib/motion/project/template/ios.rb:51:in `open'
/Library/RubyMotion/lib/motion/project/template/ios.rb:51:in `entries'
/Library/RubyMotion/lib/motion/project/template/ios.rb:51:in `block in pre_build_actions'
/Library/RubyMotion/lib/motion/project/template/ios.rb:50:in `each'
/Library/RubyMotion/lib/motion/project/template/ios.rb:50:in `pre_build_actions'
/Library/RubyMotion/lib/motion/project/template/ios.rb:66:in `block (2 levels) in <top (required)>'
/Library/RubyMotion/lib/motion/project/template/ios.rb:121:in `block in <top (required)>'
Tasks: TOP => build:simulator
(See full trace by running task with --trace)

lib/motion/project/template/ios.rb
@@ -48,6 +48,7 @@ namespace :build do
     # TODO this should go into a iOS specific Builder class which performs this
     # check before building.
     App.config.resources_dirs.each do |dir|
+      next unless Dir.exists?(dir)
       Dir.entries(dir).grep(/Resources/i).each do |basename|
         path = File.join(dir, basename)
         if File.directory?(path)