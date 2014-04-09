c0a64ff66de0cff62ee4f26d188741a376538b08
Watson <watson1978@gmail.com>
2014-04-09 14:15:45 +0900

    fix a build error in environment which uses Ruby 1.8
    http://hipbyte.myjetbrains.com/youtrack/issue/RM-463

--- lib/motion/project/template/ios.rb
+++ lib/motion/project/template/ios.rb
@@ -48,7 +48,7 @@ namespace :build do
     # TODO this should go into a iOS specific Builder class which performs this
     # check before building.
     App.config.resources_dirs.flatten.each do |dir|
-      next unless Dir.exists?(dir)
+      next unless File.exist?(dir)
       Dir.entries(dir).grep(/^Resources$/i).each do |basename|
         path = File.join(dir, basename)
         if File.directory?(path)
