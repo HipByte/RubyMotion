c88b386a2434e4e3fcc4725264927911a935c70f
Laurent Sansonetti <lrz@hipbyte.com>
2014-04-11 14:16:18 +0200

    move 1.9 specs to a different file, like rubyspec does

--- test/test-android/app/main.rb
+++ test/test-android/app/main.rb
@@ -8,8 +8,12 @@ class ShouldResult < Java::Lang::Object
     @obj = obj
   end
 
+  def set_expectation_result(res)
+    @res = res
+  end
+
   def ==(x)
-    if !(@obj == x)
+    if (@obj == x) != @res
       puts "Expectation failed (expected `#{@obj}' == `#{x}')"
       $expectations_failures += 1
     end
@@ -29,17 +33,30 @@ class Object
 
   def should(condition=nil)
     if condition
-      condition.call(self)
+      condition.call(self, true)
     else
       res = ShouldResult.new
       res.set_object self
+      res.set_expectation_result true
       res
     end
   end
 
+  def should_not(condition=nil)
+    if condition
+      condition.call(self, false)
+    else
+      res = ShouldResult.new
+      res.set_object self
+      res.set_expectation_result false
+      res
+    end
+  end
+
+
   def be_kind_of(klass)
-    lambda do |obj|
-      if !obj.kind_of?(klass)
+    lambda do |obj, res|
+      if obj.kind_of?(klass) != res
         puts "Expectation failed (expected `#{obj}' to be kind_of? `#{klass}')"
         $expectations_failures += 1
       end
@@ -48,9 +65,19 @@ class Object
   end
 
   def equal(obj2)
-    lambda do |obj|
-      if !(obj.equal?(obj2))
+    lambda do |obj, res|
+      if obj.equal?(obj2) != res
         puts "Expectation failed (expected `#{obj}' to be equal? `#{klass}')"
+        $expectations_failures += 1
+      end
+      $expectations_total += 1
+    end
+  end
+
+  def respond_to(sel)
+    lambda do |obj, res|
+      if obj.respond_to?(sel) != res
+        puts "Expectation failed (expected `#{obj}' to respond_to? `#{sel}')"
         $expectations_failures += 1
       end
       $expectations_total += 1
