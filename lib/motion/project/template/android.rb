a192545b12116b4409dc13539247854deae29d8a
Laurent Sansonetti <lrz@hipbyte.com>
2014-04-16 14:12:47 +0200

    log errors from AndroidRuntime (ex. app crashing due to an uncaught exception)

--- lib/motion/project/template/android.rb
+++ lib/motion/project/template/android.rb
@@ -303,7 +303,7 @@ def run_apk(mode)
       exit 0
     end
     # Show logs.
-    sh "\"#{adb_path}\" #{adb_mode_flag(mode)} logcat -s #{App.config.package_path}:I"
+    sh "\"#{adb_path}\" #{adb_mode_flag(mode)} logcat -s #{App.config.package_path}:I AndroidRuntime:E"
   end
 end
 
