c537374a7516cd6a6813f7f413d73624f158afde
Laurent Sansonetti <lrz@hipbyte.com>
2014-04-20 20:43:31 +0200

    add core/float/{<, <=, >, >=} specs

---
+++ test/test-android/app/core/float/gte_spec.rb
@@ -0,0 +1,7 @@
+describe "Float#>=" do
+  it "returns true if self is greater than or equal to other" do
+    (5.2 >= 5.2).should == true
+    (9.71 >= 1).should == true
+    (5.55382 >= 0xfabdafbafcab).should == false
+  end
+end
