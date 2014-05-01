34f1862d89b72f67580a6630311c320f33510579
Eloy Durán <eloy.de.enige@gmail.com>
2014-05-01 16:57:10 +0200

    [BS] Fix CoreMIDI metadata on platforms that have 64-bit support.
    These types are all defined as typedefs for UInt32 (via MIDIObjectRef).
    
    Fixes: http://hipbyte.myjetbrains.com/youtrack/issue/RM-485

--- test/test/spec/coremidi_spec.rb
+++ test/test/spec/coremidi_spec.rb
@@ -1,11 +1,27 @@
+# We test these mainly because gen_bridge_metadata doesn't produce the metadata
+# for the various types on all archs, notably 64-bit.
 describe "CoreMidi" do
-  xit "can list devices" do
-    # This actually crashes.
-    #n = MIDIGetNumberOfDevices()
-    #n.should > 0
-    #n.times do |i|
-      #device = MIDIGetDevice(i)
-      #device.class.should == MIDIDeviceRef
-    #end
+  it "can list devices" do
+    n = MIDIGetNumberOfDevices()
+    n.should > 0
+    n.times do |i|
+      device = MIDIGetDevice(i)
+      device.class.should == ((bits == 32) ? MIDIDeviceRef : Fixnum)
+    end
+  end
+
+  it "can create types" do
+    clientPointer = Pointer.new(MIDIClientRef.type)
+    MIDIClientCreate('Client', nil, nil, clientPointer)
+    clientRef = clientPointer[0]
+    clientRef.class.should == ((bits == 32) ? MIDIClientRef : Fixnum)
+
+    outputPointer = Pointer.new(MIDIPortRef.type)
+    MIDIOutputPortCreate(clientRef, 'Output', outputPointer)
+    outputRef = outputPointer[0]
+    outputRef.class.should == ((bits == 32) ? MIDIPortRef : Fixnum)
+
+    MIDIPortDispose(outputRef)
+    MIDIClientDispose(clientRef)
   end
 end
