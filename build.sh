25975ec040b6f6c64fdd1dbe8b54d607bce15e30
Laurent Sansonetti <lrz@hipbyte.com>
2014-06-02 22:27:06 -0700

    update for xcode 6

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   ./build.sh clean:vm
 #   ./build.sh
 rake $*
-SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode51-Beta5.app/Contents/Developer/Platforms rake $*
+SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode6-Beta.app/Contents/Developer/Platforms rake $*
