496420a84e3978e6be5f613be9cf82835d705129
Watson <watson1978@gmail.com>
2014-06-09 12:15:15 +0900

    [test] add spec for RM-512

--- test/test/spec/shortcut_spec.rb
+++ test/test/spec/shortcut_spec.rb
@@ -4,6 +4,9 @@ class TestShortcut
   def setFoo(x)
     @foo = x
   end
+  def setFoo1(x)
+    @foo = x
+  end
 end
 
 describe "Shortcut method on RubyObject" do
@@ -12,6 +15,14 @@ describe "Shortcut method on RubyObject" do
     obj = TestShortcut.new
     obj.respond_to?(:foo=).should == true
     obj.foo = 42
+    obj.foo.should == 42
+  end
+
+  it "should work even if method name has trailing number" do
+    # RM-512
+    obj = TestShortcut.new
+    obj.respond_to?(:foo1=).should == true
+    obj.foo1 = 42
     obj.foo.should == 42
   end
 end
