7575d9b734d8f8d7ec92cd4a0ffd3a80f9a84d9a
Watson <watson1978@gmail.com>
2014-07-22 09:11:36 +0900

    Xcode6-Beta4

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   ./build.sh clean:vm
 #   ./build.sh
 rake $*
-SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode6-Beta3.app/Contents/Developer/Platforms rake $*
+SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode6-Beta4.app/Contents/Developer/Platforms rake $*
