af4c519d811917d29ee4286ac6612310fbfa9934
Mark Villacampa <markvjal@gmail.com>
2014-08-27 22:20:19 +0200

    Honor xcode_dir in targets

--- lib/motion/project/target/extension_target.rb
+++ lib/motion/project/target/extension_target.rb
@@ -172,6 +172,7 @@ PLIST
       [
         "RM_TARGET_SDK_VERSION=\"#{@config.sdk_version}\"",
         "RM_TARGET_DEPLOYMENT_TARGET=\"#{@config.deployment_target}\"",
+        "RM_TARGET_XCODE_DIR=\"#{@config.xcode_dir}\"",
         "RM_TARGET_BUILD=\"1\""
       ].join(' ')
     end
