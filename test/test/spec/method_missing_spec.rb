887790f9e80fff5f9355cfdbccc8f6bbe1437e03
Watson <watson1978@gmail.com>
2014-09-06 13:53:17 +0900

    [test] add more spec for method_missing

--- test/test/spec/method_missing_spec.rb
+++ test/test/spec/method_missing_spec.rb
@@ -5,12 +5,23 @@ describe "method_missing" do
       method_name.should == :"meth:test:"
       args.should == [42, "foo"]
     end
+
+    def lol(*args)
+      args.should == [42, {test: "foo"}]
+      return 123
+    end
   end
 
   it "should receive non hash object with keyword argument" do
     obj = TestMethodMissingWithKeywordArgs.new
     obj.meth(42, test:"foo")
     obj.meth(42, test:"foo")
+  end
+
+  it "should not called if method is defined without keyword argument" do
+    obj = TestMethodMissingWithKeywordArgs.new
+    obj.lol(42, test:"foo").should == 123
+    obj.lol(42, test:"foo").should == 123
   end
 
   class TestMethodMissingWithNoKeywordArgs
