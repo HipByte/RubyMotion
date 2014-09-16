278cd9b35e8968c932f47c5aa5ba8a5381c798b9
Watson <watson1978@gmail.com>
2014-09-16 23:39:41 +0900

    [test] add spec for RM-601

--- test/test/spec/module_spec.rb
+++ test/test/spec/module_spec.rb
@@ -31,6 +31,18 @@ describe "Module" do
     end
   end
 
+  module BaseModule2
+    def testMethod1(obj)
+      $module2_method_called = true
+      super
+    end
+    def testMethod2(obj)
+      $module2_method_called = true
+      super
+    end
+  end
+
+
   class TestRM583 < TestModuleInclude
     include BaseModule
   end
@@ -39,5 +51,20 @@ describe "Module" do
   it "included module method should be call correctly from Objc" do
     TestRM583.new.run_testMethod2.should == 456
     TestRM583.new.run_testMethod1.should == 123
+  end
+
+  class TestRM601 < TestModuleInclude
+    include BaseModule, BaseModule2
+  end
+
+  # RM-601
+  it "included module methods should be call correctly from Objc if included some module" do
+    $module2_method_called = false
+    TestRM601.new.run_testMethod2.should == 456
+    $module2_method_called.should == true
+
+    $module2_method_called = false
+    TestRM601.new.run_testMethod1.should == 123
+    $module2_method_called.should == true
   end
 end
