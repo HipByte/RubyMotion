eb962797926f8c3b700ab26ad6df5ecadc92b7b2
Watson <watson1978@gmail.com>
2014-09-25 11:02:30 +0900

    use Xcode 6.1 beta2 for OS X 10.10

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   ./build.sh clean:vm
 #   ./build.sh
 rake $*
-SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode6-Beta7.app/Contents/Developer/Platforms rake $*
+SDK_BETA=1 XCODE_PLATFORMS_DIR=/Applications/Xcode-Beta.app/Contents/Developer/Platforms rake $*
