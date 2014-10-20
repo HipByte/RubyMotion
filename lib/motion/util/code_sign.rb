2aa327576ad8b2799ad60c485c1dd3601f1f7ddf
Eloy Durán <eloy.de.enige@gmail.com>
2014-10-20 16:53:19 +0200

    Some small YARD doc fixes.

--- lib/motion/util/code_sign.rb
+++ lib/motion/util/code_sign.rb
@@ -3,10 +3,10 @@
 module Motion; module Util
   module CodeSign
     class << self
-      # @param valid_only [Boolean] Whether or not to include only valid code
-      #                             sign identities.
+      # @param [Boolean] valid_only
+      #        Whether or not to include _only_ valid code sign identities.
       #
-      # @returns [String] The raw output from querying the `security` DB.
+      # @return [String] The raw output from querying the `security` DB.
       #
       def query_security_db_for_identities(valid_only)
         command = '/usr/bin/security -q find-identity -p codesigning'
@@ -14,20 +14,20 @@ module Motion; module Util
         `#{command}`.strip
       end
 
-      # @param valid_only [Boolean] Whether or not to include only valid code
-      #                             sign identities.
+      # @param [Boolean] valid_only
+      #        Whether or not to include _only_ valid code sign identities.
       #
-      # @returns [Hash{String => String}] The UUIDs and names of the identities.
+      # @return [Hash{String => String}] The UUIDs and names of the identities.
       #
       def identities(valid_only)
         output = query_security_db_for_identities(valid_only)
         Hash[*output.scan(/(\h{40})\s"(.+?)"/).flatten]
       end
 
-      # @param valid_only [Boolean] Whether or not to include only valid code
-      #                             sign identities.
+      # @param [Boolean] valid_only
+      #        Whether or not to include _only_ valid code sign identities.
       #
-      # @returns [Array<String>] The names of the identities.
+      # @return [Array<String>] The names of the identities.
       #
       def identity_names(valid_only)
         identities(valid_only).values
