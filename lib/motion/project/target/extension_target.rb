3099f848e4ac35de5a93e5cc798f0d02d994acfc
Eloy Durán <eloy.de.enige@gmail.com>
2014-12-04 17:03:20 +0100

    [Target] Configure the bundle ID in the config object only.

--- lib/motion/project/target/extension_target.rb
+++ lib/motion/project/target/extension_target.rb
@@ -37,19 +37,6 @@ module Motion; module Project
       if !File.exist?(extension_path) or File.mtime(src_path) > File.mtime(extension_path)
         App.info 'Copy', src_path
         FileUtils.cp_r(src_path, dest_path)
-
-        # At build time Extensions do not know the bundle indentifier of its
-        # parent app, so we have to modify their Info.plist later
-        extension_dir = File.join(destination_dir, extension_name)
-        info_plist = File.join(extension_dir, 'Info.plist')
-        extension_bundle_name = `/usr/libexec/PlistBuddy -c "print CFBundleName" "#{info_plist}"`.strip
-        extension_bundle_indentifer = `/usr/libexec/PlistBuddy -c "print CFBundleIdentifier" "#{info_plist}"`.strip
-        if extension_bundle_indentifer.include?(".watchkitextension")
-          extension_bundle_indentifer = "#{@config.identifier}.watchkitextension"
-        else
-          extension_bundle_indentifer = "#{@config.identifier}.#{extension_bundle_name}"
-        end
-        `/usr/libexec/PlistBuddy -c "set CFBundleIdentifier #{extension_bundle_indentifer}" "#{info_plist}"`
       end
     end
 
