2f3f3eee1872804a7b2232100ae22143133b44e2
Watson <watson1978@gmail.com>
2014-12-19 23:24:23 +0900

    remove `i386' at :build:simulator task

--- lib/motion/project/template/ios/config.rb
+++ lib/motion/project/template/ios/config.rb
@@ -52,9 +52,8 @@ module Motion; module Project;
 
     def archs
       @archs ||= begin
-        # No longer build for i386 and armv7s by default.
+        # No longer build for armv7s by default.
         archs = super
-        archs['iPhoneSimulator'].delete('i386') if archs['iPhoneSimulator'].include?('x86_64')
         archs['iPhoneOS'].delete('armv7s')
         archs
       end
