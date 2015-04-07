f0da6e30e1108f398747064cb17bf0abe5788fa2
digitalfx <digitalfx@users.noreply.github.com>
2015-04-07 11:42:38 +0100

    Add host app version to environment variables
    For use in Watch App

--- lib/motion/project/target.rb
+++ lib/motion/project/target.rb
@@ -147,6 +147,7 @@ module Motion; module Project
         "RM_TARGET_DEPLOYMENT_TARGET" => @config.deployment_target,
         "RM_TARGET_XCODE_DIR" => @config.xcode_dir,
         "RM_TARGET_HOST_APP_NAME" => @config.name,
+        "RM_TARGET_HOST_APP_VERSION" => @config.version,
         "RM_TARGET_HOST_APP_IDENTIFIER" => @config.identifier,
         "RM_TARGET_HOST_APP_PATH" => File.expand_path(@config.project_dir),
         "RM_TARGET_BUILD" => '1',
