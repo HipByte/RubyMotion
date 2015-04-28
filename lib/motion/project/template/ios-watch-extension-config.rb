c97c43a13a0e6e921e10151d92518da6a3c06239
digitalfx <digitalfx@users.noreply.github.com>
2015-04-28 23:33:03 +0100

    Fixes UIDeviceFamily to work on simulator
    Last change only worked on iOS and broke simulator support.

--- lib/motion/project/template/ios-watch-extension-config.rb
+++ lib/motion/project/template/ios-watch-extension-config.rb
@@ -171,8 +171,8 @@ EOS
       #
       def merged_info_plist(platform)
         plist = super
+        plist['UIDeviceFamily'] = (platform == 'iPhoneOS') ? [4] : plist['UIDeviceFamily'] << 4 # Probably means Apple Watch device?
         plist['CFBundleDisplayName'] = ENV['RM_TARGET_HOST_APP_NAME']
-        plist['UIDeviceFamily'] = [4] # Probably means Apple Watch device?
         plist['WKWatchKitApp'] = true
         plist['WKCompanionAppBundleIdentifier'] = ENV['RM_TARGET_HOST_APP_IDENTIFIER']
         plist.delete('UIBackgroundModes')
