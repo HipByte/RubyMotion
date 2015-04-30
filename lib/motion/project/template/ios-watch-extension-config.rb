12ea9208ae018c051f58fe08d4800d8d00773f58
Watson <watson1978@gmail.com>
2015-04-30 11:33:23 +0900

    [WatchKit] sync CFBundleDisplayName with host app
    see https://github.com/HipByte/RubyMotion/pull/214

--- lib/motion/project/template/ios-watch-extension-config.rb
+++ lib/motion/project/template/ios-watch-extension-config.rb
@@ -170,6 +170,7 @@ EOS
       #
       def merged_info_plist(platform)
         plist = super
+        plist['CFBundleDisplayName'] = ENV['RM_TARGET_HOST_APP_NAME']
         plist['UIDeviceFamily'] << 4 # Probably means Apple Watch device?
         plist['WKWatchKitApp'] = true
         plist['WKCompanionAppBundleIdentifier'] = ENV['RM_TARGET_HOST_APP_IDENTIFIER']
