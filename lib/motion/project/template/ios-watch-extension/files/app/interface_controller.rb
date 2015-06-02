d920f89800b4163123b70556bc0b3efabda4c7db
Mark Villacampa <markvjal@gmail.com>
2015-06-02 08:24:30 +0200

    Updates watch template files to match Xcode 6.3

--- lib/motion/project/template/ios-watch-extension/files/app/interface_controller.rb
+++ lib/motion/project/template/ios-watch-extension/files/app/interface_controller.rb
@@ -1,23 +1,25 @@
 class InterfaceController < WKInterfaceController
 
-  def initWithContext(context)
+  def awakeWithContext(context)
     super
 
     # Initialize variables here.
     # Configure interface objects here.
-    NSLog("%@ initWithContext", self)
+    NSLog("%@ awakeWithContext", self)
 
-    return self
+    self
   end
 
   def willActivate
     # This method is called when watch view controller is about to be visible to user
     NSLog("%@ will activate", self)
+    super
   end
 
   def didDeactivate
     # This method is called when watch view controller is no longer visible
     NSLog("%@ did deactivate", self)
+    super
   end
 
 end
