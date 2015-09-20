ce11dc5a2c44dd860b0ef200c53e7ae15a45d1a9
Mark Villacampa <markvjal@gmail.com>
2015-09-20 15:58:27 +0200

    [tvOS] Add initial template

---
+++ lib/motion/project/template/tvos-app/builder.rb
@@ -0,0 +1,138 @@
+# encoding: utf-8
+
+# Copyright (c) 2012, HipByte SPRL and contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+require 'motion/project/builder'
+
+module Motion; module Project
+  class Builder
+    def archive(config)
+      # Create .ipa archive.
+      app_bundle = config.app_bundle('iPhoneOS')
+      archive = config.archive
+      if !File.exist?(archive) or File.mtime(app_bundle) > File.mtime(archive)
+        App.info 'Create', archive
+        tmp = "/tmp/ipa_root"
+        sh "/bin/rm -rf #{tmp}"
+        sh "/bin/mkdir -p #{tmp}/Payload"
+        sh "/bin/cp -r \"#{app_bundle}\" #{tmp}/Payload"
+        sh "/bin/chmod -R 755 #{tmp}/Payload"
+        if watchapp?(config)
+          source = File.join(config.sdk('iPhoneOS'), "/Library/Application Support/WatchKit/WK")
+          sh "/usr/bin/ditto -rsrc '#{source}' #{tmp}/WatchKitSupport/WK"
+        end
+        Dir.chdir(tmp) do
+          dirs = Dir.glob("*").join(' ')
+          sh "/usr/bin/zip -q -r archive.zip #{dirs}"
+        end
+        sh "/bin/cp #{tmp}/archive.zip \"#{archive}\""
+      end
+
+      # Create manifest file (if needed).
+      manifest_plist = File.join(config.versionized_build_dir('iPhoneOS'), 'manifest.plist')
+      manifest_plist_data = config.manifest_plist_data
+      if manifest_plist_data and (!File.exist?(manifest_plist) or File.mtime(config.project_file) > File.mtime(manifest_plist))
+        App.info 'Create', manifest_plist
+        File.open(manifest_plist, 'w') { |io| io.write(manifest_plist_data) }
+      end
+    end
+
+    def codesign(config, platform)
+      bundle_path = config.app_bundle(platform)
+      raise unless File.exist?(bundle_path)
+
+      # Codesign targets
+      unless config.targets.empty?
+        config.targets.each do |target|
+          target.codesign(platform)
+        end
+      end
+
+      # Create bundle/ResourceRules.plist.
+      resource_rules_plist = File.join(bundle_path, 'ResourceRules.plist')
+      unless File.exist?(resource_rules_plist)
+        App.info 'Create', resource_rules_plist
+        File.open(resource_rules_plist, 'w') do |io|
+          io.write(<<-PLIST)
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>rules</key>
+        <dict>
+                <key>.*</key>
+                <true/>
+                <key>Info.plist</key>
+                <dict>
+                        <key>omit</key>
+                        <true/>
+                        <key>weight</key>
+                        <real>10</real>
+                </dict>
+                <key>ResourceRules.plist</key>
+                <dict>
+                        <key>omit</key>
+                        <true/>
+                        <key>weight</key>
+                        <real>100</real>
+                </dict>
+        </dict>
+</dict>
+</plist>
+PLIST
+        end
+      end
+
+      # Copy the provisioning profile.
+      bundle_provision = File.join(bundle_path, "embedded.mobileprovision")
+      App.info 'Create', bundle_provision
+      FileUtils.cp config.provisioning_profile, bundle_provision
+
+      # Codesign.
+      codesign_cmd = "CODESIGN_ALLOCATE=\"#{File.join(config.xcode_dir, 'Toolchains/XcodeDefault.xctoolchain/usr/bin/codesign_allocate')}\" /usr/bin/codesign"
+
+      app_frameworks = File.join(config.app_bundle(platform), 'Frameworks')
+      config.embedded_frameworks.each do |framework|
+        framework_path = File.join(app_frameworks, File.basename(framework))
+        if File.mtime(config.project_file) > File.mtime(framework_path) \
+            or !system("#{codesign_cmd} --verify \"#{framework_path}\" >& /dev/null")
+          App.info 'Codesign', framework_path
+          sh "#{codesign_cmd} -f -s \"#{config.codesign_certificate}\" --preserve-metadata=\"identifier,entitlements,resource-rules\" \"#{framework_path}\""
+        end
+      end
+
+      if File.mtime(config.project_file) > File.mtime(bundle_path) \
+          or !system("#{codesign_cmd} --verify \"#{bundle_path}\" >& /dev/null")
+        App.info 'Codesign', bundle_path
+        entitlements = File.join(config.versionized_build_dir(platform), "Entitlements.plist")
+        File.open(entitlements, 'w') { |io| io.write(config.entitlements_data) }
+        sh "#{codesign_cmd} -f -s \"#{config.codesign_certificate}\" --resource-rules=\"#{resource_rules_plist}\" --entitlements #{entitlements} \"#{bundle_path}\""
+      end
+    end
+
+    def watchapp?(config)
+      Dir.glob("#{config.app_bundle('iPhoneOS')}/**/_WatchKitStub/").size > 0
+    end
+  end
+end; end
---
+++ lib/motion/project/template/tvos-app/config.rb
@@ -0,0 +1,224 @@
+# encoding: utf-8
+
+# Copyright (c) 2012, HipByte SPRL and contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+require 'motion/project/template/ios/config'
+
+module Motion; module Project;
+  class TVOSConfig < IOSConfig
+    register :'tvos-app'
+
+    def initialize(*)
+      super
+      # @frameworks = ['WatchKit', 'Foundation']
+    end
+
+    # def name
+    #   @name = ENV['RM_TARGET_HOST_APP_NAME'] + ' WatchKit Extension'
+    # end
+
+    # def name=(name)
+    #   App.fail 'You cannot give a Watch application a custom name, it will ' \
+    #            'automatically be named after the host application.'
+    # end
+
+    # TODO datadir should not depend on the template name
+    def datadir(target=deployment_target)
+      File.join(motiondir, 'data', 'tvos', target)
+    end
+
+    # TODO datadir should not depend on the template name
+    def supported_versions
+      @supported_versions ||= Dir.glob(File.join(motiondir, 'data', 'tvos', '*')).select{|path| File.directory?(path)}.map do |path|
+        File.basename path
+      end
+    end
+
+    def platforms
+      %W{AppleTVSimulator AppleTVOS}
+    end
+
+    def local_platform
+      'AppleTVSimulator'
+    end
+
+    def deploy_platform
+      'AppleTVOS'
+    end
+
+    # @return [String] The bundle identifier of the watch extension based on the
+    #         bundle identifier of the host application.
+    #
+    # def identifier
+    #   if watchV2?
+    #     ENV['RM_TARGET_HOST_APP_IDENTIFIER'] + '.watchkitapp.watchkitextension'
+    #   else
+    #     ENV['RM_TARGET_HOST_APP_IDENTIFIER'] + '.watchkitextension'
+    #   end
+    # end
+
+    # @see {XcodeConfig#merged_info_plist}
+    #
+    def merged_info_plist(platform)
+      plist = super
+      plist['UIDeviceFamily'] = [3]
+      plist['UIRequiredDeviceCapabilities'] = ['arm64']
+      plist
+    end
+
+    # def cflags(platform, cplusplus)
+    #   cflags = super
+    #   if platform == 'WatchOS'
+    #     cflags += ' -fembed-bitcode'
+    #   end
+    #   cflags
+    # end
+
+    # def ldflags(platform)
+    #   ldflags = super
+    #   if platform == 'WatchOS'
+    #     ldflags += ' -fembed-bitcode -Xlinker -bitcode_hide_symbols'
+    #     ldflags.gsub!(' -Wl,-no_pie', '')
+    #   end
+    #   ldflags
+    # end
+    #
+    def cflag_version_min(platform)
+      flag = " -mtvos-version-min=#{deployment_target}"
+      if platform == "AppleTVSimulator"
+        flag = " -mtvos-simulator-version-min=#{deployment_target}"
+      end
+      flag
+    end
+
+    def bridgesupport_cflags
+      a = sdk_version.scan(/(\d+)\.(\d+)/)[0]
+      sdk_version_headers = ((a[0].to_i * 10000) + (a[1].to_i * 100)).to_s
+      "-mtvos-version-min=#{sdk_version} -D__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__=#{sdk_version_headers}"
+    end
+
+    def main_cpp_file_txt(spec_objs)
+      main_txt = <<EOS
+#import <UIKit/UIKit.h>
+
+extern "C" {
+    void rb_define_global_const(const char *, void *);
+    void rb_rb2oc_exc_handler(void);
+    void RubyMotionInit(int argc, char **argv);
+EOS
+      if spec_mode
+        spec_objs.each do |_, init_func|
+          main_txt << "void #{init_func}(void *, void *);\n"
+        end
+      end
+      main_txt << <<EOS
+}
+EOS
+
+      if spec_mode
+        main_txt << <<EOS
+@interface SpecLauncher : NSObject
+@end
+
+#include <dlfcn.h>
+
+@implementation SpecLauncher
+
++ (id)launcher
+{
+    [UIApplication sharedApplication];
+
+    // Enable simulator accessibility.
+    // Thanks http://www.stewgleadow.com/blog/2011/10/14/enabling-accessibility-for-ios-applications/
+    NSString *simulatorRoot = [[[NSProcessInfo processInfo] environment] objectForKey:@"IPHONE_SIMULATOR_ROOT"];
+    if (simulatorRoot != nil) {
+        void *appSupportLibrary = dlopen([[simulatorRoot stringByAppendingPathComponent:@"/System/Library/PrivateFrameworks/AppSupport.framework/AppSupport"] fileSystemRepresentation], RTLD_LAZY);
+        CFStringRef (*copySharedResourcesPreferencesDomainForDomain)(CFStringRef domain) = (CFStringRef (*)(CFStringRef)) dlsym(appSupportLibrary, "CPCopySharedResourcesPreferencesDomainForDomain");
+
+        if (copySharedResourcesPreferencesDomainForDomain != NULL) {
+            CFStringRef accessibilityDomain = copySharedResourcesPreferencesDomainForDomain(CFSTR("com.apple.Accessibility"));
+
+            if (accessibilityDomain != NULL) {
+                CFPreferencesSetValue(CFSTR("ApplicationAccessibilityEnabled"), kCFBooleanTrue, accessibilityDomain, kCFPreferencesAnyUser, kCFPreferencesAnyHost);
+                CFRelease(accessibilityDomain);
+            }
+        }
+    }
+
+    // Load the UIAutomation framework.
+    dlopen("/Developer/Library/PrivateFrameworks/UIAutomation.framework/UIAutomation", RTLD_LOCAL);
+
+    SpecLauncher *launcher = [[self alloc] init];
+    [[NSNotificationCenter defaultCenter] addObserver:launcher selector:@selector(appLaunched:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    return launcher;
+}
+
+- (void)appLaunched:(id)notification
+{
+    // unregister observer to avoid duplicate invocation
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+    // Give a bit of time for the simulator to attach...
+    [self performSelector:@selector(runSpecs) withObject:nil afterDelay:0.3];
+}
+
+- (void)runSpecs
+{
+EOS
+        spec_objs.each do |_, init_func|
+          main_txt << "#{init_func}(self, 0);\n"
+        end
+        main_txt << "[NSClassFromString(@\"Bacon\") performSelector:@selector(run) withObject:nil];\n"
+        main_txt << <<EOS
+}
+
+@end
+EOS
+      end
+      main_txt << <<EOS
+int
+main(int argc, char **argv)
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retval = 0;
+EOS
+    if ENV['ARR_CYCLES_DISABLE']
+      main_txt << <<EOS
+    setenv("ARR_CYCLES_DISABLE", "1", true);
+EOS
+    end
+    main_txt << "[SpecLauncher launcher];\n" if spec_mode
+    main_txt << <<EOS
+    RubyMotionInit(argc, argv);
+EOS
+    main_txt << <<EOS
+    retval = UIApplicationMain(argc, argv, nil, @"#{delegate_class}");
+    exit(retval);
+    [pool release];
+    return retval;
+}
+EOS
+    end
+
+  end
+end; end
