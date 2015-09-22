bd0e0cf393b70c2409ed24ec6c6408f5db97eac5
Mark Villacampa <markvjal@gmail.com>
2015-09-22 21:52:22 +0200

    Add support for Android 23 and remove references to MNC

--- lib/motion/project/template/android/config.rb
+++ lib/motion/project/template/android/config.rb
@@ -330,7 +330,7 @@ module Motion; module Project;
             '5'
           when '10', '11'
             '9'
-          when '22', '23', 'MNC'
+          when '22', '23'
             '21'
           else
             api_version
