dc0902df8cf86369a65f47fe696b9cec9e4ccb69
Watson <watson1978@gmail.com>
2015-11-20 11:02:29 +0900

    Revert "not embed kernel-*.bc into each compiled files"
    This reverts commit b257909cb79937ccac35258f3cd250a31ce4ba6b.
    
    Because we cannot use getLazyBitcodeModule() in RubyMotion core.

--- lib/motion/project/builder.rb
+++ lib/motion/project/builder.rb
@@ -353,8 +353,7 @@ EOS
         objs_file = Tempfile.new('linker-objs-list')
         objs_list.each { |obj| objs_file.puts(obj) }
         objs_file.close # flush
-        kernel = File.join(datadir, platform, "kernel.o")
-        sh "#{cxx} -o \"#{main_exec}\" \"#{kernel}\" -filelist \"#{objs_file.path}\" #{config.ldflags(platform)} -L#{File.join(datadir, platform)} -lrubymotion-static -lobjc -licucore #{linker_option} #{framework_search_paths} #{frameworks} #{weak_frameworks} #{config.libs.join(' ')} #{vendor_libs}"
+        sh "#{cxx} -o \"#{main_exec}\" -filelist \"#{objs_file.path}\" #{config.ldflags(platform)} -L#{File.join(datadir, platform)} -lrubymotion-static -lobjc -licucore #{linker_option} #{framework_search_paths} #{frameworks} #{weak_frameworks} #{config.libs.join(' ')} #{vendor_libs}"
         main_exec_created = true
 
         # Change the install name of embedded frameworks.
--- lib/motion/project/template/ios-extension-builder.rb
+++ lib/motion/project/template/ios-extension-builder.rb
@@ -337,10 +337,9 @@ EOS
             "-stdlib=libc++"
           end
         end || ""
-        kernel = File.join(datadir, platform, "kernel.o")
         # Use the `-no_implicit_dylibs` linker option to hide the fact that it
         # links against `libextension.dylib` which contains `NSExtensionMain`.
-        sh "#{cxx} -o \"#{main_exec}\" \"#{kernel}\" #{objs_list}  -fobjc-link-runtime -fapplication-extension -Xlinker -no_implicit_dylibs #{config.ldflags(platform)} -L#{File.join(datadir, platform)} -lrubymotion-static -lobjc -licucore #{linker_option} #{framework_search_paths} #{frameworks} #{weak_frameworks} #{config.libs.join(' ')} #{vendor_libs}"
+        sh "#{cxx} -o \"#{main_exec}\" #{objs_list}  -fobjc-link-runtime -fapplication-extension -Xlinker -no_implicit_dylibs #{config.ldflags(platform)} -L#{File.join(datadir, platform)} -lrubymotion-static -lobjc -licucore #{linker_option} #{framework_search_paths} #{frameworks} #{weak_frameworks} #{config.libs.join(' ')} #{vendor_libs}"
         main_exec_created = true
       end
 
