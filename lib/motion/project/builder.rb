f48af9f0bccaaa7cd591f46d5ca7a8dcf5b36a6f
Mark Villacampa <m@markvillacampa.com>
2016-06-09 02:38:47 +0200

    Generate dSYM every time the executable is linked

--- lib/motion/project/builder.rb
+++ lib/motion/project/builder.rb
@@ -539,7 +539,7 @@ EOS
       end
 
       # Generate dSYM.
-      if any_obj_file_built
+      if main_exec_created
         dsym_path = config.app_bundle_dsym(platform)
         FileUtils.rm_rf(dsym_path)
         App.info "Create", dsym_path
