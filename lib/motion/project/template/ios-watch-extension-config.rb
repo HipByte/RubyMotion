2a88a55e2ee3bf69e563431f9e526aad00e9e448
Watson <watson1978@gmail.com>
2016-06-27 15:17:48 +0900

    use ‘+=‘ for self-assginment

--- lib/motion/project/template/ios-watch-extension-config.rb
+++ lib/motion/project/template/ios-watch-extension-config.rb
@@ -57,7 +57,7 @@ module Motion; module Project;
     #
     def name
       @name = ENV['RM_TARGET_HOST_APP_NAME']
-      @name = @name + (watchV2? ? ' WatchKit Extension' : ' WatchKit 1 Extension')
+      @name += (watchV2? ? ' WatchKit Extension' : ' WatchKit 1 Extension')
     end
 
     def name=(name)
@@ -230,7 +230,7 @@ EOS
 
         if ENV['RM_TARGET_HOST_APP_NAME']
           @name = ENV['RM_TARGET_HOST_APP_NAME']
-          @name = @name + (watchV2? ? ' WatchKit App' : ' WatchKit 1 App')
+          @name += (watchV2? ? ' WatchKit App' : ' WatchKit 1 App')
         end
         @version = ENV['RM_TARGET_HOST_APP_VERSION']
         @short_version = ENV['RM_TARGET_HOST_APP_SHORT_VERSION']
