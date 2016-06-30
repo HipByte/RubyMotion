925460ea79e2e6d7eec6f546447451e3867949f7
Watson <watson1978@gmail.com>
2016-06-30 11:14:54 +0900

    [Xcode 8] recognize with path of Xcode 8 beta for 'rake profile'
    Since Xcode 8 beta, the Xcode path changed form Xcode-Beta.app to Xcode-beta.app

--- lib/motion/project/xcode_config.rb
+++ lib/motion/project/xcode_config.rb
@@ -365,7 +365,7 @@ module Motion; module Project;
         else
           App.fail("Invalid Instruments template path or name.")
         end
-        if !xcode_dir.include?("-Beta.app")
+        unless xcode_dir.downcase.include?("xcode-beta.app")
           # workaround for RM-599, RM-672 and RM-832. Xcode 6.x beta doesn't need this workaround
           template_path = File.expand_path("#{xcode_dir}/../Applications/Instruments.app/Contents/Resources/templates/#{template_path}.tracetemplate")
         end
