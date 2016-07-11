a037c1a4c4130234adb9ee789cbecc6ff90d14cd
Watson <watson1978@gmail.com>
2016-07-12 00:38:49 +0900

    [Xcode 8] remove patch which display spec log on device
    This patch does not work as expected with Xcode 8 beta 2.
    
    Since RubyMotion 4.12, print method (ie, "puts"/"print", etc) use NSLog when app run on device

--- lib/motion/spec.rb
+++ lib/motion/spec.rb
@@ -799,15 +799,3 @@ end
 
 # Do not log all exceptions when running the specs.
 Exception.log_exceptions = false
-
-# FIXME : Need better detection for iPhone Simulator
-if defined?(UIDevice) && UIDevice.respond_to?("currentDevice") && !(UIDevice.currentDevice.name =~ /(iPhone|iPad) Simulator/)
-  module Kernel
-    def puts(*args)
-      NSLog(args.join("\n"))
-    end
-    def print(*args)
-      puts *args # TODO
-    end
-  end
-end
