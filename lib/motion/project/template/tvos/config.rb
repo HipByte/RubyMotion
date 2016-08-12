d92fd808f83122ae6eba6f7ac46fe38401267bcd
Watson <watson1978@gmail.com>
2016-08-12 22:02:20 +0900

    [tvOS] should use __ENVIRONMENT_TV_XXX (not IPHONE) as cflags

--- lib/motion/project/template/tvos/config.rb
+++ lib/motion/project/template/tvos/config.rb
@@ -137,7 +137,7 @@ module Motion; module Project
     def bridgesupport_cflags
       a = sdk_version.scan(/(\d+)\.(\d+)/)[0]
       sdk_version_headers = ((a[0].to_i * 10000) + (a[1].to_i * 100)).to_s
-      "-mtvos-version-min=#{sdk_version} -D__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__=#{sdk_version_headers}"
+      "-mtvos-version-min=#{sdk_version} -D__ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__=#{sdk_version_headers}"
     end
 
     def main_cpp_file_txt(spec_objs)
