e71ab3f5f9bf7ed15df999656db923c4debbb9b2
Mark Villacampa <m@markvillacampa.com>
2016-10-28 18:00:25 +0200

    [iOS Extensions] add default ui for notification content extension

--- lib/motion/project/template/ios-notification-content-extension/files/app/notification_view_controller.rb
+++ lib/motion/project/template/ios-notification-content-extension/files/app/notification_view_controller.rb
@@ -2,7 +2,20 @@ class NotificationViewController < UIViewController
   # Enable 'IB' if you want to use storyboard.
   # extend IB
 
+  attr_accessor :label
+
   def viewDidLoad
+    self.view.backgroundColor = UIColor.redColor
+
+    self.label = UILabel.alloc.init
+    label.text = "Hello World"
+    label.textColor = UIColor.whiteColor
+    label.textAlignment = NSTextAlignmentCenter
+    label.setTranslatesAutoresizingMaskIntoConstraints(false)
+    self.view.addSubview(label)
+
+    self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-[label]-|", options:0, metrics:nil, views:{ "label" => label }))
+    self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[label]-|", options:0, metrics:nil, views:{ "label" => label }))
   end
 
   def didReceiveNotification(notification)
