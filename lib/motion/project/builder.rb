8a5d89a1acffde2018142a2099da18729a74e367
Watson <watson1978@gmail.com>
2016-10-31 01:33:27 +0900

    remove redundant `return'

--- lib/motion/project/builder.rb
+++ lib/motion/project/builder.rb
@@ -568,12 +568,12 @@ EOS
 
     def convert_filesystem_encoding(string)
       if RUBY_VERSION < "2.1.0"
-        return eval `\"#{@nfd}\" "#{string}"`
+        eval `\"#{@nfd}\" "#{string}"`
       else
         # Dir.glob on Ruby 2.1 returns file path as "Normalization Form C".
         # So, we do not convert to "Normalization Form D".
         # (Ruby 2.0 or below, Dir.glob returns "Normalization Form D").
-        return string
+        string
       end
     end
 
